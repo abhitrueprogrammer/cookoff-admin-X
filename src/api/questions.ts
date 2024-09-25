6ecbb25d8bea53fca30042dab6dc38e40d39d197
Abh1noob <abhinavganeshank@gmail.com>
2024-09-25 14:22:05 +0530

    reference

---
+++ src/api/questions.ts
@@ -0,0 +1,13 @@
+//Refer GET request and POST request from ./getTODO.ts and ./createPost.ts
+
+//Define API related interfaces here
+
+//CreateQuestion
+
+//UpdateQuestion
+
+//DeleteQuestion
+
+//GetAllQuestions
+
+//GetQuestionById
