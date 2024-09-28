75f8be509dfbe1ab9f082b0f87d91337c69b1f4c
abhitrueprogrammer <abhiaruvs3@gmail.com>
2024-09-28 19:11:11 +0530

    fixed build

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,11 +2,11 @@
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import { FaHome } from "react-icons/fa";
-function dashboard() {
+function Dashboard() {
     const router = useRouter()
   return (
     <div>
-        <Button onClick={()=>{ router}} className="bg-bb m-4 hover:bg-slate-950" ><FaHome size={20} className="text-white"></FaHome></Button>
+        <Button onClick={()=>{ router.push("/dashboard")}} className="bg-bb m-4 hover:bg-slate-950" ><FaHome size={20} className="text-white"></FaHome></Button>
         <div className="flex h-screen w-screen items-center justify-around gap-20">
           <Button onClick={()=>{router.push("/question")}} className=" m-10 flex-grow h-80 border-2 p-15 border-orange-500 text-6xl">QUESTIONS</Button>
           <Button onClick={()=>{router.push("/users")}} className="m-10 flex-grow h-80 border-2 p-15 border-orange-500 text-6xl">USERS</Button>
@@ -15,4 +15,4 @@ function dashboard() {
   );
 }
 
-export default dashboard;
+export default Dashboard;
