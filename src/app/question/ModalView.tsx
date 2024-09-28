c90a9a2e2eeee07394ca1f5bdebc99213dc3252b
abhitrueprogrammer <abhiaruvs3@gmail.com>
2024-09-28 12:55:36 +0530

    fixed view more

--- src/app/question/ModalView.tsx
+++ src/app/question/ModalView.tsx
@@ -19,23 +19,25 @@ const ModalDetails = ({
   return (
     <Dialog open={isModalOpen} onOpenChange={setModalOpen}>
       <DialogTrigger asChild>{children}</DialogTrigger>
-      <DialogContent className="overflow-visible p-0 sm:max-w-fit">
-        <div className="border-b px-6 py-4">
+      <DialogContent className="overflow-hidden break-words p-0 sm:max-w-7xl">
+        <div className="border-b px-6 py-4 sm:max-w-4xl">
           <h3 className="font-medium capitalize">Details</h3>
         </div>
-        <div className="flex flex-col-reverse md:flex-row">
+        <div className="flex flex-col-reverse md:flex-row " >
           <div className="flex flex-col justify-between md:border-r">
-            <div className="space-y-1 border-t p-6 md:border-none">
+            <div className="space-y-3 border-t p-6 md:border-none sm:max-w-7xl">
               <h4 className="text-md font-medium">Info</h4>
               <ModalDetailText label="Title: " content={row.original.Title} />
               <div className="flex flex-row items-center">
                 <ModalDetailText label="ID: " content={row.original.ID} />
                 <CopyButton content={row.original.ID} />
               </div>
-              <ModalDetailText
-                label="Description: "
-                content={row.original.Description}
-              />
+              {/* <ModalDetailText
+                label="Description: " */}
+                
+                <div className="break-words text-sm">Discription: {row.original.Description}</div>
+                
+              {/* /> */}
               <ModalDetailText
                 label="Input Format: "
                 content={row.original.InputFormat?.join("\n")}
@@ -56,10 +58,9 @@ const ModalDetails = ({
                 label="Sample Input: "
                 content={row.original.SampleTestInput?.join("\n")}
               />
-              <ModalDetailText
-                label="Sample Explanation: "
-                content={row.original.Explanation?.join("\n")}
-              ></ModalDetailText>
+
+                <div className="break-words text-sm">Explanation: {row.original.Explanation?.join("\n")}</div>
+
               <ModalDetailText
                 label="Sample Output: "
                 content={row.original.SampleTestOutput?.join("\n")}
