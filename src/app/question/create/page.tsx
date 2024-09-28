deaad7d80a0f066f7391af505efa9092f3aaee49
Abh1noob <abhinavganeshank@gmail.com>
2024-09-29 04:27:07 +0530

    masti: 6

--- src/app/question/create/page.tsx
+++ src/app/question/create/page.tsx
@@ -6,33 +6,20 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-// import { useCreateBlockNote } from "@blocknote/react";
-// import { BlockNoteView } from "@blocknote/mantine";
+import { ApiError } from "next/dist/server/api-utils";
 import dynamic from "next/dynamic";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { FaHome } from "react-icons/fa";
+import { getData } from "./editor";
 
 const Editor = dynamic(() => import("./editor"), { ssr: false });
 
-// import "@blocknote/mantine/style.css";
-
-import { ApiError } from "next/dist/server/api-utils";
-import { useForm } from "react-hook-form";
-import toast from "react-hot-toast";
-
-import { useRouter } from "next/navigation";
-import { FaHome } from "react-icons/fa";
-import { getData } from "./editor";
-// interface CreateButtonProps {
-//     questions: QuestionResponse[];
-//     setQuestions: React.Dispatch<React.SetStateAction<QuestionResponse[]>>;
-//   }
-
 const Create = () => {
-  // const editor = useCreateBlockNote();
   const router = useRouter();
-
   const queryClient = useQueryClient();
-  // Creates a new editor instance.
-  // const editor = useCreateBlockNote();
 
   const {
     register,
@@ -41,25 +28,26 @@ const Create = () => {
     reset,
   } = useForm<CreateQuestionParams>();
 
+  const [sampleInputs, setSampleInputs] = useState<string[]>([""]);
+  const [sampleOutputs, setSampleOutputs] = useState<string[]>([""]);
+  const [explanations, setExplanations] = useState<string[]>([""]);
+  const [inputFormats, setInputFormats] = useState<string[]>([""]);
+
   const createQuestion = useMutation({
     mutationFn: async (data: CreateQuestionParams) => {
-      // if (editor) {
-
       const markdown = await getData();
       if (markdown) {
         data.description = markdown;
       }
-      // }
-      data.input_format = data.input_format?.[0]?.split("\n") ?? [];
+      data.input_format = inputFormats;
       data.points = +data.points;
       data.round = +data.round;
-      // data.Round = 1;
       data.constraints = data.constraints?.[0]?.split("\n") ?? [];
       data.output_format = data.output_format?.[0]?.split("\n") ?? [];
-      data.sample_test_input = data.sample_test_input?.[0]?.split("\n") ?? [];
-      data.sample_test_output = data.sample_test_output?.[0]?.split("\n") ?? [];
-      data.sample_explanation = data.sample_explanation?.[0]?.split("\n") ?? [];
-      console.log(data.sample_explanation);
+      data.sample_test_input = sampleInputs;
+      data.sample_test_output = sampleOutputs;
+      data.sample_explanation = explanations;
+
       return toast.promise(CreateQuestion(data), {
         loading: "Adding Question",
         success: "Success!",
@@ -72,12 +60,60 @@ const Create = () => {
       router.push("/question");
     },
     onError: () => {
-      console.log("out of syllabus");
+      console.log("Error occurred while creating the question");
     },
   });
 
   const onSubmit = (data: CreateQuestionParams) => {
     createQuestion.mutate(data);
+  };
+
+  const addSampleInput = () => {
+    setSampleInputs([...sampleInputs, ""]);
+  };
+
+  const addSampleOutput = () => {
+    setSampleOutputs([...sampleOutputs, ""]);
+  };
+
+  const addExplanation = () => {
+    setExplanations([...explanations, ""]);
+  };
+
+  const addInputFormat = () => {
+    setInputFormats([...inputFormats, ""]);
+  };
+
+  const handleInputChange = (index: number, value: string, type: string) => {
+    if (type === "input") {
+      const newInputs = [...sampleInputs];
+      newInputs[index] = value;
+      setSampleInputs(newInputs);
+    } else if (type === "output") {
+      const newOutputs = [...sampleOutputs];
+      newOutputs[index] = value;
+      setSampleOutputs(newOutputs);
+    } else if (type === "explanation") {
+      const newExplanations = [...explanations];
+      newExplanations[index] = value;
+      setExplanations(newExplanations);
+    } else if (type === "format") {
+      const newFormats = [...inputFormats];
+      newFormats[index] = value;
+      setInputFormats(newFormats);
+    }
+  };
+
+  const deleteEntry = (index: number, type: string) => {
+    if (type === "input") {
+      setSampleInputs(sampleInputs.filter((_, i) => i !== index));
+    } else if (type === "output") {
+      setSampleOutputs(sampleOutputs.filter((_, i) => i !== index));
+    } else if (type === "explanation") {
+      setExplanations(explanations.filter((_, i) => i !== index));
+    } else if (type === "format") {
+      setInputFormats(inputFormats.filter((_, i) => i !== index));
+    }
   };
 
   return (
@@ -91,7 +127,7 @@ const Create = () => {
         >
           <FaHome size={20} className="text-white" />
         </Button>
-        <h1 className="s-sling font- flex-grow text-center text-2xl font-bold text-accent">
+        <h1 className="flex-grow text-center text-2xl font-bold text-accent">
           Add Questions
         </h1>
       </div>
@@ -110,6 +146,7 @@ const Create = () => {
             {...register("title")}
           />
         </div>
+
         <div className="grid grid-cols-4 items-center gap-4">
           <Label
             htmlFor="description"
@@ -117,23 +154,45 @@ const Create = () => {
           >
             Description
           </Label>
-          {/* <Editor  editor={editor}  /> */}
           <Editor />
         </div>
+
+        {/* Input Format Section */}
         <div className="grid grid-cols-4 items-center gap-4">
-          <Label
-            htmlFor="input_format"
-            className="text-right text-lg font-bold text-primary"
-          >
-            Input Format
-          </Label>
-          <Textarea
-            id="input_format"
-            placeholder="3 integers"
-            className="col-span-3"
-            {...register("input_format.0")}
-          />
+          <div className="flex flex-row items-center gap-2">
+            <Label
+              htmlFor="input_format"
+              className="col-span-3 whitespace-nowrap text-right text-lg font-bold text-primary"
+            >
+              Input Format
+            </Label>
+            <Button type="button" onClick={addInputFormat}>
+              +
+            </Button>
+          </div>
+          <div className="col-span-3 flex w-full flex-col">
+            {inputFormats.map((format, index) => (
+              <div key={index} className="flex items-center gap-2">
+                <Textarea
+                  value={format}
+                  placeholder="3 integers"
+                  className="col-span-3"
+                  onChange={(e) =>
+                    handleInputChange(index, e.target.value, "format")
+                  }
+                />
+                <Button
+                  type="button"
+                  onClick={() => deleteEntry(index, "format")}
+                  className="text-red-500"
+                >
+                  Delete
+                </Button>
+              </div>
+            ))}
+          </div>
         </div>
+
         <div className="grid grid-cols-4 items-center gap-4">
           <Label
             htmlFor="points"
@@ -149,6 +208,7 @@ const Create = () => {
             {...register("points")}
           />
         </div>
+
         <div className="grid grid-cols-4 items-center gap-4">
           <Label
             htmlFor="round"
@@ -160,7 +220,7 @@ const Create = () => {
             {...register("round")}
             defaultValue={1}
             id="round"
-            className="rounded-md border bg-gray-200 p-2 text-black"
+            className="col-span-3 rounded-md border bg-gray-200 p-2 text-black"
           >
             <option value={0}>Round 0</option>
             <option value={1}>Round 1</option>
@@ -168,6 +228,7 @@ const Create = () => {
             <option value={3}>Round 3</option>
           </select>
         </div>
+
         <div className="grid grid-cols-4 items-center gap-4">
           <Label
             htmlFor="constraints"
@@ -182,6 +243,7 @@ const Create = () => {
             {...register("constraints.0")}
           />
         </div>
+
         <div className="grid grid-cols-4 items-center gap-4">
           <Label
             htmlFor="output_format"
@@ -196,50 +258,116 @@ const Create = () => {
             {...register("output_format.0")}
           />
         </div>
+
+        {/* Sample Test Output Section */}
         <div className="grid grid-cols-4 items-center gap-4">
-          <Label
-            htmlFor="sample_test_output"
-            className="text-right text-lg font-bold text-primary"
-          >
-            Sample Test Output
-          </Label>
-          <Textarea
-            id="sample_test_output"
-            placeholder="Number"
-            className="col-span-3"
-            {...register("sample_test_output.0")}
-          />
-        </div>
-        <div className="grid grid-cols-4 items-center gap-4">
-          <Label
-            htmlFor="sample_test_input"
-            className="text-right text-lg font-bold text-primary"
-          >
-            Sample Test Input
-          </Label>
-          <Textarea
-            id="sample_test_input"
-            placeholder="Abracadabra"
-            className="col-span-3"
-            {...register("sample_test_input.0")}
-          />
-        </div>
-        <div className="grid grid-cols-4 items-center gap-4">
-          <Label
-            htmlFor="explanation"
-            className="text-right text-lg font-bold text-primary"
-          >
-            Explanation
-          </Label>
-          <Textarea
-            id="explanation"
-            placeholder="The why, the who, what, when, the where, and the how"
-            className="col-span-3"
-            {...register("sample_explanation.0")}
-          />
+          <div className="flex flex-row items-center gap-2">
+            <Label
+              htmlFor="sample_test_output"
+              className="whitespace-nowrap text-right text-lg font-bold text-primary"
+            >
+              Sample Test Output
+            </Label>
+            <Button type="button" onClick={addSampleOutput}>
+              +
+            </Button>
+          </div>
+          <div className="col-span-3 flex w-full flex-col">
+            {sampleOutputs.map((output, index) => (
+              <div key={index} className="flex items-center gap-2">
+                <Textarea
+                  value={output}
+                  placeholder="Number"
+                  className="w-full"
+                  onChange={(e) =>
+                    handleInputChange(index, e.target.value, "output")
+                  }
+                />
+                <Button
+                  type="button"
+                  onClick={() => deleteEntry(index, "output")}
+                  className="text-red-500"
+                >
+                  Delete
+                </Button>
+              </div>
+            ))}
+          </div>
         </div>
 
-        <Button className="ml-auto flex" type="submit">
+        {/* Sample Test Input Section */}
+        <div className="grid grid-cols-4 items-center gap-4">
+          <div className="flex flex-row items-center gap-2">
+            <Label
+              htmlFor="sample_test_input"
+              className="whitespace-nowrap text-right text-lg font-bold text-primary"
+            >
+              Sample Test Input
+            </Label>
+            <Button type="button" onClick={addSampleInput}>
+              +
+            </Button>
+          </div>
+          <div className="col-span-3 flex w-full flex-col">
+            {sampleInputs.map((input, index) => (
+              <div key={index} className="flex items-center gap-2">
+                <Textarea
+                  value={input}
+                  placeholder="Input"
+                  className="w-full"
+                  onChange={(e) =>
+                    handleInputChange(index, e.target.value, "input")
+                  }
+                />
+                <Button
+                  type="button"
+                  onClick={() => deleteEntry(index, "input")}
+                  className="text-red-500"
+                >
+                  Delete
+                </Button>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Sample Explanation Section */}
+        <div className="grid grid-cols-4 items-center gap-4">
+          <div className="flex flex-row items-center gap-2">
+            <Label
+              htmlFor="sample_explanation"
+              className="whitespace-nowrap text-right text-lg font-bold text-primary"
+            >
+              Sample Explanation
+            </Label>
+            <Button type="button" onClick={addExplanation}>
+              +
+            </Button>
+          </div>
+          <div className="col-span-3 flex w-full flex-col">
+            {explanations.map((explanation, index) => (
+              <div key={index} className="flex items-center gap-2">
+                <Textarea
+                  value={explanation}
+                  placeholder="Explanation"
+                  className="w-full"
+                  onChange={(e) =>
+                    handleInputChange(index, e.target.value, "explanation")
+                  }
+                />
+                <Button
+                  type="button"
+                  onClick={() => deleteEntry(index, "explanation")}
+                  className="text-red-500"
+                >
+                  Delete
+                </Button>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <Button type="submit" className="mt-4">
           Submit
         </Button>
       </form>
